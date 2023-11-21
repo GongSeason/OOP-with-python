457077553640f7c3c51da23003e65efd9c6570a5
GongSeason <2492596620@qq.com>
2023-11-21 10:50:35 +0800

    Updated assert

--- Constructro.py
+++ Constructro.py
@@ -1,9 +1,14 @@
 class Item:
-    def __init__(self, name,price,quantity) -> None:
+    def __init__(self, name:str,price:float,quantity:int) -> None:
+        # Run validation to the received arguments
+        assert price >=0, f'Price{price} is not equal or greater than zero!'
+        assert quantity>=0, f'quantity{quantity} is not equal or greater than zero!'
+
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
-        print(f"An instance created:{name}")
+        
     
     def calculate_total_price(self):
         return self.name*self.quantity
