65dc8a2246cf0a6141f09a80462e48253c7e2604
GongSeason <2492596620@qq.com>
2023-11-21 11:10:44 +0800

    updated pay_rate attribute

--- Constructro.py
+++ Constructro.py
@@ -1,4 +1,5 @@
 class Item:
+    pay_rate = 0.8 # The pay rate after 20% count
     def __init__(self, name:str,price:float,quantity:int) -> None:
         # Run validation to the received arguments
         assert price >=0, f'Price{price} is not equal or greater than zero!'
@@ -13,11 +14,16 @@ class Item:
     def calculate_total_price(self):
         return self.name*self.quantity
     
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+    
 
 item1 = Item('Phone',100,5)
 
-print(item1.calculate_total_price())
-
 item2 = Item('Laptop',1000,5)
+item2.pay_rate = 0.7
+print(item2.apply_discount())
 
-print(item2.calculate_total_price())
+print(Item.__dict__)  # All the attributes for Class level
+print(item1.__dict__) # all the attributes for instance level
+
