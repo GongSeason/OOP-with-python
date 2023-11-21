b4debfda4038832b55506eb6a1d3d58eba3ee52c
GongSeason <2492596620@qq.com>
2023-11-21 11:18:07 +0800

    updated list of all instance

--- Constructro.py
+++ Constructro.py
@@ -1,5 +1,6 @@
 class Item:
     pay_rate = 0.8 # The pay rate after 20% count
+    all=[]
     def __init__(self, name:str,price:float,quantity:int) -> None:
         # Run validation to the received arguments
         assert price >=0, f'Price{price} is not equal or greater than zero!'
@@ -9,6 +10,11 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+
+
         
     
     def calculate_total_price(self):
@@ -19,11 +25,12 @@ class Item:
     
 
 item1 = Item('Phone',100,5)
-
 item2 = Item('Laptop',1000,5)
-item2.pay_rate = 0.7
-print(item2.apply_discount())
+item3 = Item('Cable',100,5)
+item4 = Item('Mouse',1000,5)
+item5 = Item('Keyboard',100,5)
+item6 = Item('Monitor',1000,5)
 
-print(Item.__dict__)  # All the attributes for Class level
-print(item1.__dict__) # all the attributes for instance level
+for instance in Item.all:
+    print(instance.name)
 
